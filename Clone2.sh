1812a73f84189f2caa10b17fcbd09d66672c41e5
manojpawarsj12 <46449199+manojpawarsj12@users.noreply.github.com>
2019-03-05 17:24:10 +0530

    Create Clone2.sh

---
+++ Clone2.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/LineageOS/android_device_oneplus_dumpling.git -b lineage-16.0 device/oneplus/dumpling
+git clone https://github.com/LineageOS/android_device_oneplus_msm8998-common.git -b lineage-16.0 device/oneplus/msm8998-common
+git clone https://github.com/LineageOS/android_kernel_oneplus_msm8998.git -b lineage-16.0 kernel/oneplus/msm8998
+git clone https://github.com/TheMuppets/proprietary_vendor_oneplus.git -b lineage-16.0 vendor/oneplus
