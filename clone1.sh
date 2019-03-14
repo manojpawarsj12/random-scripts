53c96a4737508e8e597dc85b8fd6a025ea942756
manojpawarsj12 <46449199+manojpawarsj12@users.noreply.github.com>
2019-03-14 18:03:51 +0530

    Update clone1.sh

--- clone1.sh
+++ clone1.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/SGCMarkus/proprietary_vendor_lge.git -b lineage-16.0 vendor/lge
-git clone https://github.com/zachariasmaladroit/kernel_lge_msm8998.git -b pleroma_II_WIP2_2 kernel/lge/msm8998
+git clone https://github.com/zachariasmaladroit/kernel_lge_msm8998.git -b orion_II_WIP3_3 kernel/lge/msm8998
 git clone https://github.com/SGCMarkus/android_device_lge_joan-common.git -b lineage-16.0 device/lge/joan-common
 git clone https://github.com/SGCMarkus/android_device_lge_h930.git -b lineage-16.0  device/lge/h930
