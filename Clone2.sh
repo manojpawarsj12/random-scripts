7068df12a551ea515c85bf4dd4be46f820c0e1fe
luffy1105 <46449199+manojpawarsj12@users.noreply.github.com>
2019-05-08 13:55:04 +0530

    Update Clone2.sh

--- Clone2.sh
+++ Clone2.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/LineageOS/android_device_oneplus_dumpling.git -b lineage-16.0 device/oneplus/dumpling
-git clone https ://https://github.com/GZOSP-Devices/device_oneplus_msm8998-common -b 9.0 device/oneplus/msm8998-common
+git clone https://github.com/GZOSP-Devices/device_oneplus_msm8998-common -b 9.0 device/oneplus/msm8998-common
 git clone https://github.com/LineageOS/android_kernel_oneplus_msm8998.git -b lineage-16.0 kernel/oneplus/msm8998
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus.git -b lineage-16.0 vendor/oneplus
 git clone https://github.com/LineageOS/android_device_oppo_common.git -b lineage-16.0 device/oppo/common
