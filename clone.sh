ad7cfbc8a6d8659b439e119e9f35fa30e67e81c7
Manoj Pawar SJ <46449199+manojpawarsj12@users.noreply.github.com>
2019-10-20 12:34:58 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/SGCMarkus/proprietary_vendor_lge.git -b lineage-16.0 vendor/lge
 git clone https://github.com/SGCMarkus/android_kernel_lge_msm8998.git -b lineage-16.0 kernel/lge/msm8998
-git clone  https://github.com/manojpawarsj12/android_device_lge_joan-common -b caf device/lge/joan-common
-git clone https://github.com/manojpawarsj12/android_device_lge_h930.git -b dot-p device/lge/h930
+git clone https://github.com/SGCMarkus/android_device_lge_joan-common.git -b lineage-16.0 device/lge/joan-common
+git clone https://github.com/SGCMarkus/android_device_lge_h930.git -b lineage-16.0  device/lge/h930
